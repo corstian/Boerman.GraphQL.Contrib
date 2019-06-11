d92a3a455e5507d12a6746101217993b08ce2102
Corstian Boerman <mail@corstianboerman.com>
2019-06-11 10:53:44 +0200

    Marked the IId interface as obsolete

--- Boerman.GraphQL.Contrib/IId.cs
+++ Boerman.GraphQL.Contrib/IId.cs
@@ -2,6 +2,7 @@
 
 namespace Boerman.GraphQL.Contrib
 {
+    [Obsolete("Using this interface makes maintenance harder, and should not be a requirement anymore to use the `.ToConnection` extension method.")]
     public interface IId
     {
         Guid Id { get; set; }
